e967bc8bde62dabcbff43b018e43773ee27caee7
zihco <chizobajames21@gmail.com>
2024-06-20 15:26:30 +0100

    done

--- utils/db.js
+++ utils/db.js
@@ -5,16 +5,16 @@ class DBClient {
     const host = process.env.DB_HOST || '127.0.0.1';
     const port = process.env.DB_PORT || 27017;
     const database = process.env.DB_DATABASE || 'files_manager';
-    this.clientIsConnected = true;
+    //this.clientIsConnected = true;
     this.client = new MongoClient(`mongodb://${host}:${port}`,
       { useUnifiedTopology: true });
     this.client.connect()
       .then(() => {
         this.db = this.client.db(database);
-        this.clientIsConnected = true;
+        //this.clientIsConnected = true;
       })
       .catch(() => {
-        this.clientIsConnected = false;
+        //this.clientIsConnected = false;
       });
   }
 
@@ -23,12 +23,12 @@ class DBClient {
   }
 
   async nbUsers() {
-    const users = await this.client.db().collection('users');
+    const users = await this.client.db.collection('users');
     return users.countDocuments();
   }
 
   async nbFiles() {
-    const files = await this.client.db().collection('files');
+    const files = await this.client.db.collection('files');
     return files.countDocuments();
   }
 }
